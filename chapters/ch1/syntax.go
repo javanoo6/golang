d6ec01fa098850e0ed167e05b76f6723e1e8c491
Konkov Nikita <n.konkov@rqc.ru>
2026-02-14 17:32:20 +0300

    methods output

--- chapters/ch1/syntax.go
+++ chapters/ch1/syntax.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, Go!")
+	// fmt.Println("Hello, Go!")//
+
+	fmt.Println(math.Floor(2.75))               // 2
+	fmt.Println(strings.Title("head first go")) // Head First Go
 }
