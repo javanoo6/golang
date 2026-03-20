7be08add55fd10aaa6897e955504b966926a1689
Konkov Nikita <n.konkov@rqc.ru>
2026-03-20 21:35:27 +0300

    ch8 changes

--- chapters/ch8/main.go
+++ chapters/ch8/main.go
@@ -23,4 +23,25 @@ func main() {
 	subscriberSemiInit.Rate = 4.99
 	subscriberSemiInit.Active = true
 	fmt.Println(subscriberSemiInit.Name, subscriberSemiInit.Rate, subscriberSemiInit.Active)
+
+	newSubscriber := magazine.Subscriber{Name: "Aman Singh"}
+	newSubscriber.HomeAddress.Street = "123 Oak St"
+	newSubscriber.HomeAddress.City = "Omaha"
+	newSubscriber.HomeAddress.State = "NE"
+	newSubscriber.HomeAddress.PostalCode = "68111"
+	fmt.Println("Subscriber Name:", newSubscriber.Name)
+	fmt.Println("Street:", newSubscriber.HomeAddress.Street)
+	fmt.Println("City:", newSubscriber.HomeAddress.City)
+	fmt.Println("State:", newSubscriber.HomeAddress.State)
+	fmt.Println("Postal Code:", newSubscriber.HomeAddress.PostalCode)
+	employee := magazine.Employee{Name: "Joy Carr"}
+	employee.Street = "456 Elm St"
+	employee.City = "Portland"
+	employee.State = "OR"
+	employee.PostalCode = "97222"
+	fmt.Println("Employee Name:", employee.Name)
+	fmt.Println("Street:", employee.Street)
+	fmt.Println("City:", employee.City)
+	fmt.Println("State:", employee.State)
+	fmt.Println("Postal Code:", employee.PostalCode)
 }
--- chapters/go.mod
+++ chapters/go.mod
@@ -2,4 +2,9 @@ module chapters
 
 go 1.25.6
 
-require github.com/headfirstgo/keyboard v0.0.0-20170926053303-9930bcf72703 // indirect
+require (
+	github.com/headfirstgo/keyboard v0.0.0-20170926053303-9930bcf72703 // indirect
+	github.com/headfirstgo/magazine v0.0.0
+)
+
+replace github.com/headfirstgo/magazine => ../src/github.com/headfirstgo/magazine
